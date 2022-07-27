950315938400dce292756d559fb7686a79995534
Divyanarina <divyanarina511@gmail.com>
2022-07-27 15:29:12 +0530

    if condition

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+if[ $USER_ID -ne 0 ]
+then
+  echo you are non root user
+  echo you need to login as root user to execute.
+  fi
+
+
 #To Install Nginx.
 
 yum install nginx -y
